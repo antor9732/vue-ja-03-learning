a69e9058870f8d52a4e03deb681dc6a6e38ba58c
Md. Mahmudul Hasan <antormahmud789@gmail.com>
2024-12-12 18:57:55 +0600

    file clean

--- second-vueJS-project/vueJs-2nd-project/src/App.vue
+++ second-vueJS-project/vueJs-2nd-project/src/App.vue
@@ -4,17 +4,7 @@ import TheWelcome from "./components/TheWelcome.vue";
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="Mahmudul Hasan did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+<h1>My Name is Mahmudul Hasan</h1>
 </template>
 
 <style scoped>
---
+++ vue-JS-first-project/src/App.vue
@@ -0,0 +1,19 @@
+<script setup>
+
+</script>
+
+<template>
+  
+      <h1>Mahmudul did it!</h1>
+      
+</template>
+
+<style scoped>
+h1{
+  color: red;
+
+  font-size: 50px;
+  background: black;
+}
+
+</style>
