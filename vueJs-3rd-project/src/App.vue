00aec22fc1f790810a549e451e318b6e7e389814
Md. Mahmudul Hasan <antormahmud789@gmail.com>
2024-12-19 14:43:49 +0600

    Binding HTML Classes try

---
+++ vueJs-3rd-project/src/App.vue
@@ -0,0 +1,20 @@
+<script>
+import Counter from '@/components/Counter.vue';
+
+export default{
+  components:{ Counter },
+}
+</script>
+
+<template>
+  <Counter/>
+</template>
+
+<style>
+* {
+  margin: 0;
+  box-sizing: border-box;
+  padding: 0px;
+
+}
+</style>
